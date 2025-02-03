e9341dd25a4691729826a9f29b2c791e48bb6f76
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-02-03 15:27:08 -0300

    feat - implementation of results interactivity

--- src/script.js
+++ src/script.js
@@ -4,4 +4,16 @@ function calcFatorial(num) {
   return result;
 }
 
+let input = document.querySelector("#input");
+let resultP = document.querySelector("#result");
+let button = document.querySelector("#button");
 
+if (input.value === "") resultP.innerHTML = `Insira um valor positivo acima,
+e em seguida clique em no botão = para gerar o resultado.`;
+
+button.addEventListener("click", () => {
+  if (input.value === "") return alert("Insira um número");
+  if (input.value <= 0) return alert("Insira um número positivo");
+  resultP.innerHTML = `O fatorial de ${input.value}! é: 
+  <strong>${calcFatorial(input.value)}</strong>`;
+});
